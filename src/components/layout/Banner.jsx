75a0fec3586a08b87373460ca8b62b4ea9583a56
guilherme <guile.hm@hotmail.com>
2019-12-23 20:57:14 -0300

    rename from class to className

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -3,29 +3,29 @@ import React from 'react'
 
 const Banner = () => {
     return (
-        <div class="callbacks_container">
-            <ul class="rslides" id="slider3">
-                <li class="banner banner1">
-                    <div class="container">
-                        <div class="banner-text">
-                            <div class="slider-info">
+        <div className="callbacks_container">
+            <ul className="rslides" id="slider3">
+                <li className="banner banner1">
+                    <div className="container">
+                        <div className="banner-text">
+                            <div className="slider-info">
                                 <h3>Pet Lost</h3>
-                                <span class="line"></span>
+                                <span className="line"></span>
                                 <p>Fazendo pets mais felizes</p>
                             </div>
                         </div>
                     </div>
                 </li>
-                <li class="banner banner3">
-                    <div class="container">
-                        <div class="banner-text">
-                            <div class="slider-info">
+                <li className="banner banner3">
+                    <div className="container">
+                        <div className="banner-text">
+                            <div className="slider-info">
                                 <h3>Ajude um Pet</h3>
-                                <span class="line"></span>
+                                <span className="line"></span>
                                 <p>A encontrar sua família</p>
-                                <a class="btn bg-theme mt-4 w3_pvt-link-bnr scroll" data-blast="bgColor" href="#lost"
+                                <a className="btn bg-theme mt-4 w3_pvt-link-bnr scroll" data-blast="bgColor" href="#lost"
                                     role="button">Desaparecidos</a>
-                                <a class="btn bg-theme ml-4 mt-4 w3_pvt-link-bnr scroll" data-blast="bgColor" href="#found"
+                                <a className="btn bg-theme ml-4 mt-4 w3_pvt-link-bnr scroll" data-blast="bgColor" href="#found"
                                     role="button">Encontrados</a>
                             </div>
                         </div>
