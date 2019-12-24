980ec96af2c744035acb7fe36837e7bafb68c93a
guilherme <guile.hm@hotmail.com>
2019-12-24 12:34:30 -0300

    improve pet reducer

--- src/reducers/pets.js
+++ src/reducers/pets.js
@@ -1,5 +1,12 @@
+import {
+    FETCH_ACTIVE_PETS,
+} from '../constants/ActionTypes'
+
+
 const pets = (state = [], action) => {
     switch (action.type) {
+        case FETCH_ACTIVE_PETS:
+            return [{ ...action.payload }]
         default:
             return state
     }
