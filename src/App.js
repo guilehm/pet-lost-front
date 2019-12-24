cf796318cdce313ebf6f4bf6f54696c51a4fc974
guilherme <guile.hm@hotmail.com>
2019-12-24 12:34:58 -0300

    refactor app to use redux

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import './App.css'
 import Header from './components/layout/Header'
 import Banner from './components/layout/Banner'
 import RandomGallery from './components/layout/RandomGallery'
-import PetCardGroup from './components/pets/PetCardGroup'
+import PetList  from './containers/PetCardGroup.container'
 
 
 const App = () => (
-        <div className="App">
-            <Header />
-            <Banner />
-            <PetCardGroup />
-            <RandomGallery />
-        </div>
-    )
+    <div className="App">
+        <Header />
+        <Banner />
+        <PetList />
+        <RandomGallery />
+    </div>
+)
 
 
 export default App
