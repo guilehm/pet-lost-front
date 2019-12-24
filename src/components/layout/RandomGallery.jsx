ae1478b2de9c361386ad7ec5b8b93c38bbfaa9ad
guilherme <guile.hm@hotmail.com>
2019-12-23 21:58:30 -0300

    refactor from class to className

--- src/components/layout/RandomGallery.jsx
+++ src/components/layout/RandomGallery.jsx
@@ -3,66 +3,66 @@ import React from 'react'
 
 const RandomGallery = () => {
     return (
-        <section class="wthree-row w3-gallery cliptop-portfolio-wthree pt-lg-5" id="portfolio">
-            <div class="container-fluid">
-                <div class="title-desc text-center pb-3">
-                    <h3 class="main-title-w3pvt">Desaparecidos</h3>
+        <section className="wthree-row w3-gallery cliptop-portfolio-wthree pt-lg-5" id="portfolio">
+            <div className="container-fluid">
+                <div className="title-desc text-center pb-3">
+                    <h3 className="main-title-w3pvt">Desaparecidos</h3>
                     <p>Pets que estão sendo procurados por suas famílias.</p>
                 </div>
-                <ul class="demo row py-lg-5 py-sm-4 pb-4">
-                    <li class="col-lg-3 col-sm-6">
-                        <div class="gallery-grid1">
-                            <img src="images/g1.jpg" alt=" " class="img-fluid img-thumbnail" />
+                <ul className="demo row py-lg-5 py-sm-4 pb-4">
+                    <li className="col-lg-3 col-sm-6">
+                        <div className="gallery-grid1">
+                            <img src="images/g1.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
-                    <li class="col-lg-3 col-sm-6">
-                        <div class="gallery-grid1">
-                            <img src="images/g4.jpg" alt=" " class="img-fluid img-thumbnail" />
+                    <li className="col-lg-3 col-sm-6">
+                        <div className="gallery-grid1">
+                            <img src="images/g4.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
-                    <li class="col-lg-3 col-sm-6">
-                        <div class="gallery-grid1">
-                            <img src="images/g5.jpg" alt=" " class="img-fluid img-thumbnail" />
+                    <li className="col-lg-3 col-sm-6">
+                        <div className="gallery-grid1">
+                            <img src="images/g5.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
-                    <li class="col-lg-3 col-sm-6">
-                        <div class="gallery-grid1">
-                            <img src="images/g3.jpg" alt=" " class="img-fluid img-thumbnail" />
+                    <li className="col-lg-3 col-sm-6">
+                        <div className="gallery-grid1">
+                            <img src="images/g3.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
-                    <li class="col-lg-4 col-sm-6">
-                        <div class="gallery-grid1">
-                            <img src="images/g2.jpg" alt=" " class="img-fluid img-thumbnail" />
+                    <li className="col-lg-4 col-sm-6">
+                        <div className="gallery-grid1">
+                            <img src="images/g2.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
-                    <li class="col-lg-4 col-sm-6">
-                        <div class="gallery-grid1">
-                            <img src="images/g1.jpg" alt=" " class="img-fluid img-thumbnail" />
+                    <li className="col-lg-4 col-sm-6">
+                        <div className="gallery-grid1">
+                            <img src="images/g1.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
-                    <li class="col-lg-4 col-sm-6 mx-auto">
-                        <div class="gallery-grid1">
-                            <img src="images/g4.jpg" alt=" " class="img-fluid img-thumbnail" />
+                    <li className="col-lg-4 col-sm-6 mx-auto">
+                        <div className="gallery-grid1">
+                            <img src="images/g4.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
-                    <li class="col-lg-3 col-sm-6">
-                        <div class="gallery-grid1">
-                            <img src="images/g5.jpg" alt=" " class="img-fluid img-thumbnail" />
+                    <li className="col-lg-3 col-sm-6">
+                        <div className="gallery-grid1">
+                            <img src="images/g5.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
-                    <li class="col-lg-3 col-sm-6">
-                        <div class="gallery-grid1">
-                            <img src="images/g4.jpg" alt=" " class="img-fluid img-thumbnail" />
+                    <li className="col-lg-3 col-sm-6">
+                        <div className="gallery-grid1">
+                            <img src="images/g4.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
-                    <li class="col-lg-3 col-sm-6">
-                        <div class="gallery-grid1">
-                            <img src="images/g6.jpg" alt=" " class="img-fluid img-thumbnail" />
+                    <li className="col-lg-3 col-sm-6">
+                        <div className="gallery-grid1">
+                            <img src="images/g6.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
-                    <li class="col-lg-3 col-sm-6">
-                        <div class="gallery-grid1">
-                            <img src="images/g5.jpg" alt=" " class="img-fluid img-thumbnail" />
+                    <li className="col-lg-3 col-sm-6">
+                        <div className="gallery-grid1">
+                            <img src="images/g5.jpg" alt=" " className="img-fluid img-thumbnail" />
                         </div>
                     </li>
                 </ul>
