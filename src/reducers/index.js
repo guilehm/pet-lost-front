d92eae9062019f7f28f7bfc4263dc8e458f5a128
guilherme <guile.hm@hotmail.com>
2019-12-28 13:59:28 -0300

    create state for layout

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import activePets from './pets'
+import layout from './layout'
 import { combineReducers } from 'redux'
 
 
 const rootReducer = combineReducers({
     activePets,
+    layout,
 })
 
 
