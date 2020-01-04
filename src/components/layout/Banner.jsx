46e09bcd7f4460c4dadbe3ec8c354fe7eec449f7
guilherme <guile.hm@hotmail.com>
2020-01-04 15:36:22 -0300

    display dinamical banners

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -28,35 +28,24 @@ const Banner = ({ banners, actions }) => {
 
     return (
         <div className="callbacks_container">
-            <ul className="rslides" id="slider3">
-                <li className="banner banner1">
-                    <div className="container">
-                        <div className="banner-text">
-                            <div className="slider-info">
-                                <h3>Pet Lost</h3>
-                                <span className="line"></span>
-                                <p>Fazendo pets mais felizes</p>
-                                <a className="btn bg-theme ml-4 mt-4 w3_pvt-link-bnr scroll" data-blast="bgColor" href="#announce"
-                                    role="button">Anuncie agora</a>
+            <ul className="rslides">
+                {banners.slice(0, 1).map(banner =>
+
+                    <li className="banner" style={bannerStyle(banner.picture)}>
+                        <div className="container">
+                            <div className="banner-text">
+                                <div className="slider-info">
+                                    <h3>{banner.title}</h3>
+                                    <span className="line"></span>
+                                    <p>{banner.subtitle}</p>
+                                    {addButtons(banner)}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-                <li className="banner banner3">
-                    <div className="container">
-                        <div className="banner-text">
-                            <div className="slider-info">
-                                <h3>Ajude um Pet</h3>
-                                <span className="line"></span>
-                                <p>A encontrar sua família</p>
-                                <a className="btn bg-theme mt-4 w3_pvt-link-bnr scroll" data-blast="bgColor" href="#lost"
-                                    role="button">Desaparecidos</a>
-                                <a className="btn bg-theme ml-4 mt-4 w3_pvt-link-bnr scroll" data-blast="bgColor" href="#found"
-                                    role="button">Encontrados</a>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+                    </li>
+
+                )}
+
             </ul>
         </div>
     )
