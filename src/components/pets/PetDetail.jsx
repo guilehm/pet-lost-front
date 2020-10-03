72fc1c638832d8093620e2f7d4d0008c78278b61
guilherme <guile.hm@hotmail.com>
2020-10-03 20:29:10 -0300

    create pet detail component

--- src/components/pets/PetDetail.jsx
+++ src/components/pets/PetDetail.jsx
@@ -1,13 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
+import API from '../../services/api'
 
-const PetDetail = ({ petSlug, selectedPet, actions }) => {
+import { useParams } from 'react-router-dom'
+
+const Api = new API()
+
+const PetDetail = ({ actions }) => {
+    const { slug } = useParams()
+    const [pet, setPet] = useState('false')
+
+    const handleError = err => console.log(err)
+    const handleSuccess = res => {
+        setPet(res.data)
+    }
+    console.log('caiu aqui')
+
     useEffect(() => {
-        actions.fetchPetDetail(petSlug)
-    }, [actions, petSlug])
+        Api.getPetDetail(slug)
+            .then(handleSuccess)
+            .catch(handleError)
+    }, [slug])
 
     return (
-        <div>Hello World</div>
+        <div>{pet.name}</div>
     )
 }
 
