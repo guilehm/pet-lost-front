a6cd521b360d1db6ad02cea0d67d2c939f4d43b6
guilherme <guile.hm@hotmail.com>
2020-10-03 20:29:24 -0300

    create routes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import './App.css'
 import Header from './components/layout/Header'
 import Banner from './containers/Banner.container'
 import RandomGallery from './components/layout/RandomGallery'
-import PetList  from './containers/PetCardGroup.container'
-
+import PetList from './containers/PetCardGroup.container'
+import PetDetail from './containers/PetDetail.container'
+import NotFoundPage from './components/layout/NotFoundPage'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom"
 
 const App = () => (
-    <div className="App">
-        <Header />
-        <Banner />
-        <PetList />
-        <RandomGallery />
-    </div>
+    <Router>
+        <div className="App">
+            <Switch>
+                <Route exact path="/pets/:slug">
+                    <PetDetail />
+                </Route>
+                <Route exact path="/">
+                    <Header />
+                    <Banner />
+                    <PetList />
+                    <RandomGallery />
+                </Route>
+                <Route path='*' exact={true} component={NotFoundPage} />
+            </Switch>
+        </div>
+    </Router>
 )
-
 
 export default App
