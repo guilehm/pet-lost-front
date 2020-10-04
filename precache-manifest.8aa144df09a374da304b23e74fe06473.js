c61d196cb7860d19cf63f9a81529c7f487f9d9fb
Guilherme <guile.hm@hotmail.com>
2020-10-04 09:29:40 -0300

    Updates

---
+++ precache-manifest.8aa144df09a374da304b23e74fe06473.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a656fc6dbde426c4d0979afe365a0d5",
+    "url": "/pet-lost-front/index.html"
+  },
+  {
+    "revision": "7722ca3c923d4004f8e5",
+    "url": "/pet-lost-front/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "91dd0c64223570816986",
+    "url": "/pet-lost-front/static/js/2.b9e2e89d.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/pet-lost-front/static/js/2.b9e2e89d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7722ca3c923d4004f8e5",
+    "url": "/pet-lost-front/static/js/main.a6b3821e.chunk.js"
+  },
+  {
+    "revision": "1da4d35f62657a3af436",
+    "url": "/pet-lost-front/static/js/runtime-main.3539d3ef.js"
+  }
+]);
