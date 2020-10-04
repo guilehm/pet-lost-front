1a17ba386eb69261e6eadd7a8fc6662db8b35191
guilherme <guile.hm@hotmail.com>
2020-10-04 00:43:19 -0300

    improve footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 
 
-const Footer = () => {
-    return <React.Fragment>
-        <footer class="cpy-right bg-theme" data-blast="bgColor">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 text-lg-right mt-lg-0 mt-4">
-                        <p>Desenvolvido por
-                            <a href="https://github.com/Guilehm"> guilherme luis.</a>
+const Footer = () =>
+    <React.Fragment>
+        <footer className="cpy-custom-right bg-custom-dark" data-blast="bgColor">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6 text-lg-right mt-lg-0 mt-4">
+                        <p className="text-white">Desenvolvido por
+                            <a className="text-custom-theme" href="https://github.com/Guilehm"> guilherme luis.</a>
                         </p>
                     </div>
                 </div>
             </div>
         </footer>
     </React.Fragment>
-}
 
 
 export default Footer
