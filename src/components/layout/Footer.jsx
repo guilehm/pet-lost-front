367c6d0a6fa0db9b606cb8fb52791ab5d35f5637
guilherme <guile.hm@hotmail.com>
2020-10-04 00:31:40 -0300

    create footer component

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return <React.Fragment>
+        <footer class="cpy-right bg-theme" data-blast="bgColor">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 text-lg-right mt-lg-0 mt-4">
+                        <p>Desenvolvido por
+                            <a href="https://github.com/Guilehm"> guilherme luis.</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </React.Fragment>
+}
+
+
+export default Footer
